b07892860dafff56c85f5f7554718d8348269a22
amine <mo.amine.alt@gmail.com>
2025-08-03 11:32:10 +0100

    feat: add todo list functionality and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import { nanoid } from 'nanoid'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+type Todo = {
+  id: string;
+  todo: string;
+};
+
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const form = document.getElementById("add");
+
+    const handleSubmit = (e: Event) => {
+      e.preventDefault();
+      const target = e.target as HTMLFormElement;
+      const formData = new FormData(target);
+      const todoItem = formData.get("todo")?.toString().trim();
+
+      if (todoItem) {
+        setTodoList((prevList) => [...prevList, { id: nanoid(), todo: todoItem }]);
+      }
+
+      target.reset();
+    };
+
+    form?.addEventListener("submit", handleSubmit);
+
+    return () => {
+      form?.removeEventListener("submit", handleSubmit);
+    };
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>To do list:</h1>
+        <form action='#' id="add">
+            <label htmlFor="todo">
+                Task:
+            </label>
+            <input name="todo" type="text" id="todo"/>
+            <button type="submit">add to do</button>
+        </form>
+        <div>
+            {todoList.map((todo:Todo)=>{
+            return (
+                <div>
+                    <div key={todo.id}>{todo.todo}</div>
+                    <button onClick={()=>setTodoList((prevList:Todo[]):Todo[]=>{
+                        return prevList.filter((item)=>item.id!==todo.id)
+                    })}>remove item</button>
+                </div>
+            )
+            })}
+        </div>
+
+
     </>
   )
 }
