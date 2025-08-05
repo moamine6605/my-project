7130aa0819f2d43d98989364d769dd70f86f789a
amine <mo.amine.alt@gmail.com>
2025-08-05 16:30:37 +0100

    feat: add Todos component for managing todo items and integrate with App

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,13 @@
-import { useEffect, useState } from "react"
-import { nanoid } from 'nanoid'
 import Header from "./components/Header";
+import Todos from "./components/Todos";
 
 
 function App() {
 
-
-type Todo = {
-  id: string;
-  todo: string;
-};
-
-  const [todoList, setTodoList] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    const form = document.getElementById("add");
-
-    const handleSubmit = (e: Event) => {
-      e.preventDefault();
-      const target = e.target as HTMLFormElement;
-      const formData = new FormData(target);
-      const todoItem = formData.get("todo")?.toString().trim();
-
-      if (todoItem) {
-        setTodoList((prevList) => [...prevList, { id: nanoid(), todo: todoItem }]);
-      }
-
-      target.reset();
-    };
-
-    form?.addEventListener("submit", handleSubmit);
-
-    return () => {
-      form?.removeEventListener("submit", handleSubmit);
-    };
-  }, []);
-
-
   return (
     <>
         <Header />
-        <div>
-            {todoList.map((todo:Todo)=>{
-            return (
-                <div key={todo.id}>
-                    <div>{todo.todo}</div>
-                    <button onClick={()=>setTodoList((prevList:Todo[]):Todo[]=>{
-                        return prevList.filter((item)=>item.id!==todo.id)
-                    })}>remove item</button>
-                </div>
-            )
-            })}
-        </div>
-
-
+        <Todos />
     </>
   )
 }
---
+++ src/components/Todos.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import { nanoid } from 'nanoid'
+import { FaTrash } from 'react-icons/fa';
+
+export default function Todos() {
+
+type Todo = {
+  id: string;
+  todo: string;
+};
+
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const form = document.getElementById("add");
+
+    const handleSubmit = (e: Event) => {
+      e.preventDefault();
+      const target = e.target as HTMLFormElement;
+      const formData = new FormData(target);
+      const todoItem = formData.get("todo")?.toString().trim();
+
+      if (todoItem) {
+        setTodoList((prevList) => [...prevList, { id: nanoid(), todo: todoItem }]);
+      }
+
+      target.reset();
+    };
+
+    form?.addEventListener("submit", handleSubmit);
+
+    return () => {
+      form?.removeEventListener("submit", handleSubmit);
+    };
+  }, []);
+
+
+    return (
+        <div className="container">
+            {todoList.map((todo:Todo)=>{
+            return (
+                <div className="container-items" key={todo.id}>
+                    <p>{new Date().toDateString()}</p>
+                    <p>{todo.todo}</p>
+                    <button onClick={()=>setTodoList((prevList:Todo[]):Todo[]=>{
+                        return prevList.filter((item)=>item.id!==todo.id)
+                    })}><FaTrash /> Delete</button>
+                </div>
+            )
+            })}
+        </div>
+    )
+}
